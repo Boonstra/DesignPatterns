6cac03671e7b7b52c5e0a37ad9344a8e590c589a
Stefan Boonstra <stefanboonstra92@gmail.com>
2013-09-22 22:29:48 +0200

    Removed unused imports

--- src/nl/hanze/designpatterns/DAO/main/Main.java
+++ src/nl/hanze/designpatterns/DAO/main/Main.java
@@ -1,12 +1,7 @@
 package nl.hanze.designpatterns.DAO.main;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-
-import nl.hanze.designpatterns.DAO.impl.db.TroubleTicketDAOImpl;
 import nl.hanze.designpatterns.db.Executor;
 import nl.hanze.designpatterns.db.Executor.ExecutorException;
-import nl.hanze.designpatterns.domain.TroubleTicket;
 
 public class Main
 {
