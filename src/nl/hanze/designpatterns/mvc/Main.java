b020776de72bc142b084a3e9130309c403ccfab4
Stefan Boonstra <stefanboonstra@hotmail.com>
2013-09-24 14:04:07 +0200

    Removed old Main.java file

--- src/nl/hanze/designpatterns/mvc/Main.java
+++ src/nl/hanze/designpatterns/mvc/Main.java
@@ -12,25 +12,25 @@ import nl.hanze.designpatterns.view.View;
 
 public class Main {
 
-    public static void main(String[] args) {           
-        SwingUtilities.invokeLater(new Runnable() {
-            @Override
-            public void run() {                     
-            	Executor executor = null;
+	public static void main(String[] args) {           
+		SwingUtilities.invokeLater(new Runnable() {
+			@Override
+			public void run() {                     
+				Executor executor = null;
 				try {
 					executor = Executor.getInstance();
 				} catch (ExecutorException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
-            	   
-        	   executor.setUrl("jdbc:mysql://localhost:3306/designpatterns");
-        	   executor.setUser("root");
-        	   executor.setPassword("");
-                Model model = new Model();
-                View view = new View("-"); 
-                Controller controller = new Controller(model,view);
-            }
-        });  
-    }
+
+				executor.setUrl("jdbc:mysql://localhost:3306/designpatterns");
+				executor.setUser("root");
+				executor.setPassword("");
+				Model model = new Model();
+				View view = new View("-"); 
+				Controller controller = new Controller(model,view);
+			}
+		});  
+	}
 }
