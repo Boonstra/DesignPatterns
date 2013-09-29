7389f3f237c947ee682e9055b4a77a9a648592d9
Stefan Boonstra <stefanboonstra92@gmail.com>
2013-09-30 00:18:03 +0200

    After merge

--- src/nl/hanze/designpatterns/mvc/Main.java
+++ src/nl/hanze/designpatterns/mvc/Main.java
@@ -34,7 +34,7 @@ public class Main {
                 Model model = new Model();
                 View view = new View("-"); 
                 Controller controller = new Controller(model,view);
-                
+
                 new QuestionSerialize();
                 new QuestionDAOImpl();
             }
